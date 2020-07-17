41d895ab983457712d0aaa7853936cd8507e90b6
geonith-george <geonith98@gmail.com>
2020-07-17 12:55:07 +0530

    version 2

--- src/app/launchpad/launchpad.component.ts
+++ src/app/launchpad/launchpad.component.ts
@@ -6,8 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./launchpad.component.css']
 })
 export class LaunchpadComponent implements OnInit {
+  
+  bubble = new Audio('../sounds/bubbles.mp3');
 
-  constructor() { }
+  constructor() {  
+   }
+
+   playKick() {
+        this.bubble.play();
+  }
+ 
 
   ngOnInit() {
   }
